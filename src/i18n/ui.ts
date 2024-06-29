614e533e0f953385cb2c61695dbba4095364c210
jhangmez <jhangomez25@gmail.com>
2024-06-29 02:57:12 -0500

    COmmit inicial

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const showDefaultLang = false
+
+export const languages = {
+  en: 'English',
+  es: 'Español'
+}
+
+export const defaultLang = 'es'
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter'
+  },
+  es: {
+    'nav.home': 'Home',
+    'nav.sobre': 'Sobre mí'
+  }
+} as const
